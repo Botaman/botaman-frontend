30025acf88abdb908eda1982244881c4f29ce131
Bircan Bilici <info@biltel.com>
2017-05-30 22:53:01 +0300

    BF2 React Router Implemented

--- client/index.js
+++ client/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import App2 from './App2';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import {
+  BrowserRouter as Router,
+  Route,Switch
+} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Router >
+        <switch>
+       
+       <Route exact path='/' component={App}></Route>
+
+
+       <Route path='/App2' component={App2}></Route>
+       </switch>
+    </Router>
+     ,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
